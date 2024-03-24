905c7bce28ff205d69b6691af94b9bf74f4bf5f0
Kapilmeet Singh <kapilmeetsingh15@gmail.com>
2024-03-24 02:08:45 -0600

    initail commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1 className='text-center bg-red-700 text-4xl'>Currency Converter App</h1>
+    </>
+  )
+}
+
+export default App
